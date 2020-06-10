ce8cf77836ffe640a92714171ae99906f7fd1eef
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2020-06-10 15:01:36 -0300

    Incrementado rota para media 6/7

--- Módulo-2/Desafio-1/src/index.js
+++ Módulo-2/Desafio-1/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {promises} from "fs";
+import { promises } from "fs";
 import gradeRouter from "../routers/grade.js"
 
 const app = express();
@@ -11,8 +11,8 @@ const port = 3001;
 app.use(express.json());
 app.use("/grade", gradeRouter)
 
-  
-app.listen(port, async ()=> {
+
+app.listen(port, async () => {
   try {
     let date = new Date();
     await readFile(file, "utf8");
