2ed6b4fc7f546d454e404d3c606ff44c50acdcd0
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2020-06-23 23:57:30 -0300

    Desafio Guiado 3 adicionado[

---
+++ Módulo-3/React-desafio3/frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Spinner from './components/Spinner';
+import Candidates from './components/Candidates';
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      candidates: []
+    }
+    this.interval = null;
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      fetch("http://localhost:3001/votes")
+        .then((res) => {
+          return res.json();
+        })
+        .then((json) => {
+          this.setState({
+            candidates: json.candidates
+          })
+        })
+    }, 1000)
+  }
+
+  render() {
+    const { candidates } = this.state
+    if (candidates.length === 0) {
+      return <Spinner description="Carregando...." />
+    }
+    return (
+      <div>
+        <Header>Votação</Header>
+        <Candidates candidates={candidates} />
+      </div>
+    )
+  }
+}
