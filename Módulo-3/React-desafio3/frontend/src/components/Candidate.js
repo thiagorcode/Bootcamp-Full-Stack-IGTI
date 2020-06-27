9b9240700321e2fe132f7f2e0c7d0646f0a0bb7a
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2020-06-26 21:48:08 -0300

    update

--- Módulo-3/React-desafio3/frontend/src/components/Candidate.js
+++ Módulo-3/React-desafio3/frontend/src/components/Candidate.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Position from './Position'
+import Picture from './Picture'
+import Info from './Info'
+import Name from './Name'
+import Votes from './Votes'
+import Percentage from './Percentage'
 
 export default function Candidate({ candidate, position }) {
-  const { name, votes, percentage } = candidate
+  const { id, name, votes, percentage } = candidate
+  const imageSource = `${id}.jpg`
   return (
     <div>
       <Position>{position}</Position>
-      {name} - {votes}
+      <Picture imageSource={imageSource} description={name} />
+      <Info >
+        <Name>{name}</Name>
+        <Votes>{votes}</Votes>
+        <Percentage>{percentage}</Percentage>
+      </Info>
     </div>
   )
 }
